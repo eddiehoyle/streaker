c1b99558f3f78df21ab87bab5cfab1ce3ac0f42b
Eddie <eddiepie@gmail.com>
2018-12-27 15:56:52 -0500

    Working on CLI

--- streaker-rs/src/lib.rs
+++ streaker-rs/src/lib.rs
@@ -1,70 +1,32 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
+#![allow(unused_variables)]
 
 extern crate regex;
+
+use std::fs;
+use std::path::Path;
 
 mod range;
 mod streak;
 
+// 1. list files in directory
 //
-//pub fn get_padding(num: &str) -> Result<u32, &str> {
-//    let trimmed = &num.trim();
-//    if let Ok(_) = trimmed.parse::<u32>() {
-//        return Ok(trimmed.len() as u32);
-//    }
-//    Err("String was not a number.")
-//}
-//
-//pub fn to_token(padding: u32) -> String {
-//    let hashes = (0..(padding / 4)).map(|_| "#").collect::<String>();
-//    let ats = (0..(padding % 4)).map(|_| "@").collect::<String>();
-//    let token = format!("{}{}", hashes, ats);
-//    token
-//}
-//
-//#[cfg(test)]
-//mod tests {
-//    // Note this useful idiom: importing names from outer (for mod tests) scope.
-//    use super::*;
-//
-//    #[test]
-//    fn test_token_hashes() {
-//        assert_eq!(to_token(4), "#");
-//    }
-//
-//    #[test]
-//    fn test_token_ats() {
-//        assert_eq!(to_token(3), "@@@");
-//    }
-//
-//    #[test]
-//    fn test_token_both() {
-//        assert_eq!(to_token(5), "#@");
-//    }
-//
-//    #[test]
-//    fn test_padding() {
-//        assert_eq!(get_padding("0001").unwrap(), 4);
-//    }
-//
-//    #[test]
-//    fn test_max() {
-//        assert_eq!(get_padding("123").unwrap(), 3);
-//    }
-//
-//    #[test]
-//    fn test_whitespace() {
-//        assert_eq!(get_padding("    123").unwrap(), 3);
-//    }
-//
-//    #[test]
-//    fn test_negative() {
-//        assert!(get_padding("-554").is_err());
-//    }
-//
-//
-//    #[test]
-//    fn test_malformed() {
-//        assert!(get_padding("butt").is_err());
-//    }
-//}
+
+fn list_directory(directory: &Path) {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_list_files() {
+        let resource_dir = Path::new("/Users/eddiehoyle/Code/rust/streaker/resources");
+        let paths = fs::read_dir(resource_dir).unwrap();
+
+
+}
+
+}
--- streaker-rs/src/main.rs
+++ streaker-rs/src/main.rs
@@ -1,65 +1,69 @@
-//#![allow(dead_code)]
-//#![allow(unused_variables)]
-//
-//extern crate streaker;
-//extern crate clap;
-//
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+
+extern crate streaker;
+extern crate clap;
+extern crate regex;
+
 //mod range;
 //mod streak;
-//mod pattern;
-//
-//use clap::{App, Arg};
-//use std::env;
-//use std::path;
-//use std::fs;
-//
 
-fn main() {}
+use clap::{App, Arg};
+use std::env;
+use regex::Regex;
+use std::path::{Path};
+use std::fs;
 
-//fn main() {
-//
-//    let matches = App::new("Streaker")
-//        .version("1.0")
-//        .author("Eddie Hoyle")
-//        .about("Search for file sequences")
-//        .arg(Arg::with_name("directory")
-//            .short("d")
-//            .index(1)
-//            .required(true))
-//        .get_matches();
-//
-//
-//    if let Some(dir) = matches.value_of("directory") {
-//        let path = env::current_dir()
-//            .unwrap()
-//            .join(dir);
-//        if let Ok(full_directory_path) = fs::canonicalize(path) {
-//            if full_directory_path.is_dir() {
-//                seek_directory(full_directory_path.as_path());
+
+fn main() {
+
+    let matches = App::new("Streaker")
+        .version("1.0")
+        .author("Eddie Hoyle")
+        .about("Search for file sequences")
+        .arg(Arg::with_name("directory")
+            .short("d")
+            .index(1)
+            .required(true))
+        .get_matches();
+
+
+    if let Some(dir) = matches.value_of("directory") {
+        let path = env::current_dir()
+            .unwrap()
+            .join(dir);
+        if let Ok(full_directory_path) = fs::canonicalize(path) {
+            if full_directory_path.is_dir() {
+                print!("Scanning directory: {}", full_directory_path.to_str().unwrap());
+                seek_directory(full_directory_path.as_path());
+            }
+        };
+    }
+}
+
+fn seek_directory(path: &Path) {
+    println!("Querying: {}", path.to_str().unwrap());
+    if let Ok(files) = fs::read_dir(path) {
+        for file in files {
+            let file = file.unwrap();
+            let re = Regex::new(r"^(?P<name>\d+).(?P<frame>\d+).(?P<ext>\d+)$").unwrap();
+            if let Some(cap) = re.captures(file.file_name().to_str().unwrap()) {
+                let name = cap.name("name").unwrap();
+                let frame = cap.name("frame").unwrap();
+                let ext = cap.name("ext").unwrap();
+                print!("{} {} {}", name, frame, ext);
+            }
+//            let re = Regex::new(r"(\d+)").unwrap();
+//            print!("{}", file.file_name().to_str().unwrap());
+//            for mat in re.find_iter(file.file_name().to_str().unwrap()) {
+//                print!(", {}", mat.as_str());
 //            }
-//        }
-//
-////        let abs_path = fs::canonicalize(path);
-////        match abs_path {
-////            Ok(p) => {
-////                let s = seek(&p.as_path());
-////                println!("Streak: {}", s);
-////            },
-////            Err(_) => {
-////                eprintln!("Path does not exist: {}", dir);
-////                process::exit(1);
-////            }
-////        };
-//    }
-//}
-//
-//fn seek_directory(path: &path::Path) -> Vec<streak::Streak> {
-//    println!("Querying: {}", path.to_str().unwrap());
-//    if let Ok(files) = fs::read_dir(path) {
-//        let files: Vec<_> = files.collect();
-//    }
-//    Vec::new()
-//}
+//            print!("\n");
+        }
+    }
+}
+
 ////
 ////fn seek(path: &path::Path) -> Streak {
 ////
